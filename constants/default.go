e07dedbc266a4fb95f4432424b5cfd7848a46e96
simon <networm6@foxmail.com>
2024-02-20 11:42:35 +0800

    update: version code update: new path(run,file)

--- constants/default.go
+++ constants/default.go
@@ -2,9 +2,11 @@ package constants
 
 const (
 	Name    = "ProcessIsolator"
-	Version = "24.2.18"
+	Version = "24.2.20"
 	Desc    = "A tool used for running specified process in isolated environment. Enjoy it, just for fun."
 
 	Port        = "0.0.0.0:9963"
 	ProcLogPath = "/run/" + Name + "/logs/%s.log"
+	ProcRunPath = "/run/" + Name + "/runs/%s"
+	FilePath    = "/run/" + Name + "/files/%s"
 )
